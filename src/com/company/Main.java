7a94e6749d776d1049c0e0a07d30dfde329716da
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-29 21:17:10 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int total = 0;
+            int grade;
+            int average;
+            int counter = 0;
+
+            while(counter <10){
+                grade = input.nextInt();
+                total = total + grade;
+                counter++;
+            }
+            average = total/10;
+        System.out.println("your avg is "+ average);
+    }
+}
